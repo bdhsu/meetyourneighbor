48bafb3a003b4bd0276ba0969b1c1d1237dd170c
Justin Wong <justinryanwong@berkeley.edu>
2018-11-03 19:14:22 -0700

    Modified login

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ import Home from './Home';
 import Login from './Login';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = ({
-      user: null,
-    });
-    this.authListener = this.authListener.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = ({
+            user: null,
+        });
+        this.authListener = this.authListener.bind(this);
+    }
 
-  componentDidMount() {
-    this.authListener();
-  }
+    componentDidMount() {
+        this.authListener();
+    }
+    
+    authListener() {
+        fire.auth().onAuthStateChanged((user) => {
+            console.log(user);
+            if (user) {
+                this.setState({ user });
+                localStorage.setItem('user', user.uid);
+            } else {
+                this.setState({ user: null });
+                localStorage.removeItem('user');
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+            { (this.state.user) ? ( <Home/>) : (<Login />)};
+            </div>
+        )}
+    }
 
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if (user) {
-        this.setState({ user });
-        localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({ user: null });
-        localStorage.removeItem('user');
-      }
-    });
-  }
-  render() {
-    return (
-     <div>
-     { (this.state.user) ? ( <Home/>) : (<Login />)};
-     </div>
-    )}
-}
-
- export default App;
+    export default App;
--- src/Personal.js
+++ src/Personal.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import fire from './config/Fire';
 import './App.css';
 
-class Home extends Component {
+class Personal extends Component {
     constructor(props) {
         super(props);
         this.logout = this.logout.bind(this);
-        //this.personal = this.personal.bind(this);
     }
 
 
@@ -17,11 +16,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <script>
-                    var user;
-                    user= fire.auth().UserInfo#displayName;
-                </script>
-                <h1>Welcome to Home <script>user</script> Hi</h1>
+                <h1>Welcome Back this.personal</h1>
                 
                 <button onClick={this.logout}> Logout</button>
             </div>
