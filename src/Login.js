0bf16ff4ce6e76d420eda0357936c029e789aef7
Justin Wong <justinryanwong@berkeley.edu>
2018-11-03 21:01:31 -0700

    workable home

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import fire from './config/Fire';
-import './App.css';
-import Signup from './Signup.js'
-
-// This is the front Log In page. It should only include Email, Password, and Login and Signup Buttons.
-class Login extends Component {
-
+ class Login extends Component {
   constructor(props) {
     super(props);
     this.login = this.login.bind(this);
@@ -16,25 +11,19 @@ class Login extends Component {
       email: '',
       password: ''
     };
-    this.state = ({
-        user: null,
-    });
   }
-
-  handleChange(e) {
+   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
-}
-
-  // Call Firebases' signinWithEmailAndPassword function and pass in email and password
-  login(e) {
-        e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{}).catch((error) => {
-        console.log(error);
-    });
-    // Bring up LOG in screen
   }
-
-  // Creates user with specificed email and password
+   // Call Firebases' signinWithEmailAndPassword function and pass in email and password
+  login(e) {
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).catch((error) => {
+        console.log(error);
+      });
+  }
+   // Creates user with specificed email and password
   signup(e){
     e.preventDefault();
     fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
@@ -42,35 +31,27 @@ class Login extends Component {
     .catch((error) => {
         console.log(error);
       })
-      // Brings up Sign up page
-    (this.state.user) ? ( <Signup/>) : (<Login />);
   }
-
-
+   
   render() {
     return (
-        <div id="parent">
-            <div id="child" className="container col-md-6">
-                <form>
-                    <h1>Welcome to [NAME]</h1>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Email" />
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                    </div>
-
-                    <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
-                    <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
-                </form>
-            </div>
-        </div>
+       <div className="col-md-6">
+       <form>
+      <div class="form-group">
+       <label for="exampleInputEmail1">Email address</label>
+       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+       <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
+      <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
+ </form>
+ 
+ </div>
     );
-}
-
+  }
 }
 export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -11,11 +11,18 @@ class Signup extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
     this.state = {
-      email: '',
-      password: ''
-    };
-    this.state = ({
         user: null,
+        email: '',
+        password: ''
+    };
+    this.location = ({
+        location: null,
+    });
+    this.social = ({
+        social: null,
+    });
+    this.activity = ({
+        activity: null,
     });
   }
 
@@ -23,27 +30,18 @@ class Signup extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  // Call Firebases' signinWithEmailAndPassword function and pass in email and password
-  login(e) {
-    e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).catch((error) => {
-        console.log(error);
-      });
-    // Bring up LOG in screen
-  }
+//   // Creates user with specificed email and password
+//   signup(e){
+//     e.preventDefault();
+//     fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+//     }).then((u)=>{console.log(u)})
+//     .catch((error) => {
+//         console.log(error);
+//       })
 
-  // Creates user with specificed email and password
-  signup(e){
-    e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
-      })
-      // Brings up Sign up page
-    (this.state.user) ? ( <Signup/>) : (<Login />);
-  }
+//     // Brings up Sign up page
+//     (this.state.user) ? ( <Signup/>) : (<Login />);
+//   }
 
 
   render() {
@@ -51,21 +49,63 @@ class Signup extends Component {
         <div className="col-md-6">
             <form>
                 <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input  value={this.state.email} 
-                        onChange={this.handleChange} 
-                        type="email" name="email" 
-                        class="form-control" 
-                        id="exampleInputEmail1" 
-                        aria-describedby="emailHelp" 
-                        placeholder={this.state.email}  />
-       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
+                    <script> {this.state.user } 
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input  value={this.state.email} 
+                            onChange={this.handleChange} 
+                            type="email" name="email" 
+                            class="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp" 
+                            placeholder={this.state.email}  />
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
     
-       <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input  value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password" 
+                            name="password"
+                            class="form-control" 
+                            id="exampleInputPassword1"
+                            placeholder="Password" />
+                </div>
+
+                <div class="form-group">
+                    <label for="humor1">Humor</label>
+                    <input  value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password" 
+                            name="password"
+                            class="form-control" 
+                            id="humorscore1"
+                            placeholder="Rate yoself" />
+                </div>
+
+
+                <div class="form-group">
+                    <label for="humor1">Humor</label>
+                    <input  value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password" 
+                            name="password"
+                            class="form-control" 
+                            id="humorscore1"
+                            placeholder="Rate yoself" />
+                </div>
+
+                 <div class="form-group">
+                    <label for="humor1">Humor</label>
+                    <input  value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password" 
+                            name="password"
+                            class="form-control" 
+                            id="humorscore1"
+                            placeholder="Rate yoself" />
+                </div>
+
       <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
       <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
  </form>
